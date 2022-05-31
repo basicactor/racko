fd6e5a29803c0596cdf94f867473ae33cb4fc6e9
basicactor <careergo357@gmail.com>
2022-06-01 01:27:23 +0900

    vuetify追加

--- app.vue
+++ app.vue
@@ -3,3 +3,15 @@
     <NuxtPage />
   </div>
 </template>
+
+<style lang="scss">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
