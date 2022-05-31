93345a2e6a10693bb644499be5e6814ad6064062
basicactor <careergo357@gmail.com>
2022-06-01 01:41:08 +0900

    Revert "SCSS追加"
    This reverts commit 1ebc424fa9969c0322ff933b7f0a0de0e2deb240.

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,4 @@
-<script setup lang="ts">
-const show = ref(false)
-</script>
-
 <template>
-  <div id="demo">
-    <button @click="show = !show">
-      Toggle
-    </button>
 
-    <transition name="fade">
-      <p v-if="show">hello</p>
-    </transition>
-  </div>
-
+  This is index page
 </template>
-
-<style lang="scss" scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
