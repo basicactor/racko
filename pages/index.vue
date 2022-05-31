1ebc424fa9969c0322ff933b7f0a0de0e2deb240
basicactor <careergo357@gmail.com>
2022-06-01 00:56:21 +0900

    SCSS追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,28 @@
-<template>
+<script setup lang="ts">
+const show = ref(false)
+</script>
 
-  This is index page
+<template>
+  <div id="demo">
+    <button @click="show = !show">
+      Toggle
+    </button>
+
+    <transition name="fade">
+      <p v-if="show">hello</p>
+    </transition>
+  </div>
+
 </template>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
